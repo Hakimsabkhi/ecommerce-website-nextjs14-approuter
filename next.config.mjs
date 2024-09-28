3dd5dd516c68619527ff46f98b18e7e26feeb7d9
bayoudh <mohamed.salah.bayoudh@gmail.com>
2024-09-28 17:00:06 +0100

    fix eurrr mail

--- next.config.mjs
+++ next.config.mjs
@@ -11,6 +11,10 @@ const nextConfig = {
       },
       webpack: (config, { isServer }) => {
         if (isServer) {
+          // Ensure externals is defined and is an array
+          if (!config.externals) {
+            config.externals = [];
+          }
           config.externals.push('nodemailer');
         }
         return config;
